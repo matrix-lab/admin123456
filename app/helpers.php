18ec8c06eaaee3d3b536ebf683df8320326402ea
Hui Lee <lihui870920@gmail.com>
2018-09-30 23:42:24 +0800

    滚动懒加载

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,6 @@
 if (!function_exists('avatar')) {
     function avatar($email)
     {
-        return 'https://secure.gravatar.com/avatar/'.md5($email).'?size=72';
+        return 'https://secure.gravatar.com/avatar/'.md5($email).'?size=48';
     }
 }
