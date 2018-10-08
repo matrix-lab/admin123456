8605da31dbc1e429098c55cad5a617e5b62e684f
Hui Lee <lihui870920@gmail.com>
2018-10-08 09:17:26 +0800

    添加钉钉机器人通知

---
+++ app/Extension/Robot/WebHook.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Extension\Robot;
+
+class WebHook
+{
+    protected $url = 'https://oapi.dingtalk.com/robot/send?access_token=28b048d25585eabe002ad1b5ac1d6d77497f19ac9e92d65951216731c46ec13e';
+
+    public static function send($msg, $to)
+    {
+    }
+}
