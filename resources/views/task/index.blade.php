8aeab8060fda52293412f51a060a67df975b5e3c
Hui Lee <lihui870920@gmail.com>
2018-10-09 14:50:35 +0800

    添加任务ID

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -277,7 +277,7 @@
           , title: '任务清单'
           , cellMinWidth: 80
           , cols: [[
-            {field: 'id', width: 50, align: 'center', title: 'ID', sort: true}
+            {fixed: 'left', field: 'id', width: 50, align: 'center', title: 'ID', sort: true}
             , {field: 'team_alias', width: 100, align: 'center', title: '团队', sort: true}
             , {field: 'product', width: 150, align: 'center', title: '产品'}
             , {field: 'come_from', width: 100, align: 'center', title: '来源'}
