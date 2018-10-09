adac4f68ddcbfa0b20606e000306f26fe64a1d4d
Hui Lee <lihui870920@gmail.com>
2018-10-09 08:57:42 +0800

    任务明细最小列300PX

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -283,7 +283,7 @@
             , {field: 'category', width: 80, align: 'center', title: '模块'}
             , {
               field: 'content',
-              minWidth: 200,
+              minWidth: 300,
               align: 'center',
               title: '任务',
               edit: 'text',
